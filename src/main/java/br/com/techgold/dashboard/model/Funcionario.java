90e857f769698763704f2b783c82c6effcd3c294
Anderson Araujo <andersonasdj@gmail.com>
2024-09-19 14:40:35 -0300

    Ajustes de variáveis de ambiente

--- src/main/java/br/com/techgold/dashboard/model/Funcionario.java
+++ src/main/java/br/com/techgold/dashboard/model/Funcionario.java
@@ -14,7 +14,6 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -27,7 +26,6 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper=false)
 public class Funcionario extends Usuario implements UserDetails{
 	
 	private static final long serialVersionUID = 1L;
